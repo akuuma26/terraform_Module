48f7220a42f867ede9e0aa4ef9e73a268e84ccbd
ABHISEK KUMAR <diwanawelfare01@gmail.com>
2026-06-03 02:24:45 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "abhi_backend"
-    storage_account_name = "abhibackendstorage"
-    container_name       = "module-tfstate"
-    key                  = "abhiModule.terraform.tfstate"
-  }
-}
